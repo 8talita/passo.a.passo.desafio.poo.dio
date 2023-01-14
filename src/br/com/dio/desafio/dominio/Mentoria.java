51be69ce4e58e8c9de26df50c5703e557f62a39b
Talita Priscila <8talita32@gmail.com>
2023-01-14 19:04:47 -0300

    add commit

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,23 @@
+package br.com.dio.desafio.dominio;
+import java.time.LocalDate;
+public class Mentoria {
+   private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public Mentoria() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+
+    }
+
+    public void setDescricao(Mentoria Java) {
+    }
+
+}
+
